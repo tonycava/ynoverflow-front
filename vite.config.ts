6685f0bedf662ce651fd79511f90d31e45d01599
tonycava <anthony.cavagne@ynov.com>
2023-05-31 15:06:53 +0200

    add: docker dev environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		watch: {
+			usePolling: true,
+		},
+		host: true, // needed for the DC port mapping to work
+		strictPort: true,
+		port: +(process.env.PORT ?? '3000'),
+	},
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+	},
 });
